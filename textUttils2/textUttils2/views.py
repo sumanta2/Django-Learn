82fe32e32123f3168c3ff6226519e3a19e3044c6
sumanta2 <sumantagorai.21@gmail.com>
2021-11-08 21:45:26 +0530

    3 vdo6  exercise1

--- textUttils2/textUttils2/views.py
+++ textUttils2/textUttils2/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>hello form HomePage</h1>")
+    return HttpResponse("<h1>hello form Home</h1> <br> <a href='https://youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'>Django Playlist</a>")
 
 def about(request):
     return HttpResponse("Welcome to about Page")
