b9304608407acee9703534b8d84aac60df04b8cc
sumanta2 <sumantagorai.21@gmail.com>
2021-11-08 21:21:40 +0530

    2 Vdo5 ViewsAndUrl

--- textUttils2/textUttils2/urls.py
+++ textUttils2/textUttils2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'), #"" indicate the home page like '/' in php,    (views.index) it call index method inside the views file,  name paramiter indicate the name of the path
+    path('about',views.about,name='about'),
 ]
--- textUttils2/textUttils2/views.py
+++ textUttils2/textUttils2/views.py
@@ -1,2 +1,8 @@
 # This file was created by me
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("<h1>hello form HomePage</h1>")
+
+def about(request):
+    return HttpResponse("Welcome to about Page")
